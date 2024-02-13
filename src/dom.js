3c02e044c15138001e6039c52f6e882b7e831ca5
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com.>
2024-02-13 21:34:39 +0500

    Refactoring

--- src/dom.js
+++ src/dom.js
@@ -7,6 +7,9 @@ export const dom = (function () {
         const descriptionTask = document.createElement('div');
         const priorityTask = document.createElement('div');
         const dateTask = document.createElement('div');
+        const detailTaskBtn = document.createElement('button');
+        const editTaskBtn = document.createElement('button');
+        const deleteTaskBtn = document.createElement('button');
 
         showTask.className = "showTask";
         finishedTask.className = "finishedTask";
@@ -14,6 +17,9 @@ export const dom = (function () {
         descriptionTask.className = "descriptionTask";
         priorityTask.className = "priorityTask";
         dateTask.className = "dateTask";
+        detailTaskBtn.className = "detailTaskBtn";
+        editTaskBtn.className = "editTaskBtn";
+        deleteTaskBtn.className = "deleteTaskBtn";
 
         // const allTask = `${task.finished} ${task.title} ${task.description} ${task.priority} ${task.date}`;
         // showTask.textContent = allTask;
@@ -23,22 +29,73 @@ export const dom = (function () {
         descriptionTask.textContent = task.description;
         priorityTask.textContent = task.priority;
         dateTask.textContent = task.date;
+        detailTaskBtn.textContent = "Task Detail";
+        editTaskBtn.textContent = "Edit";
+        deleteTaskBtn.textContent = "Delete";
 
         showTask.appendChild(finishedTask);
         showTask.appendChild(titleTask);
         showTask.appendChild(descriptionTask);
         showTask.appendChild(priorityTask);
         showTask.appendChild(dateTask);
+        showTask.appendChild(detailTaskBtn);
+        showTask.appendChild(editTaskBtn);
+        showTask.appendChild(deleteTaskBtn);
 
         return showTask;
     };
-
+    
     const renderTodos = (tasks) => {
         const mainShow = document.querySelector('.mainShow');
         tasks.forEach(task => {
             const showTask = createTodoElement(task);
             mainShow.appendChild(showTask);
         });
+
+        detailBtn();
+        editBtn();
+        deleteBtn();
+    };
+
+    const detailBtn = () => {
+        const detailTaskBtn = document.querySelectorAll('.detailTaskBtn');
+        const taskDetailDailog = document.querySelector('.taskDetailDailog');
+        const detailCloseBtn = document.querySelector('.detailCloseBtn');
+
+        detailTaskBtn.forEach(detailTaskBtns => {
+            detailTaskBtns.addEventListener ('click', () => {
+                populateDetailDialog();
+                taskDetailDailog.showModal();
+            });
+        });
+
+        detailCloseBtn.addEventListener('click', () => {
+            taskDetailDailog.close();
+        });
+    };
+
+    const editBtn = () => {
+        const editTaskBtn = document.querySelectorAll('.editTaskBtn');
+        editTaskBtn.forEach(editTaskBtns => {
+            editTaskBtns.addEventListener('click', () => {
+                
+            });
+        });
+    }
+
+    const deleteBtn = () => {
+        const deleteTaskBtn = document.querySelectorAll('.deleteTaskBtn');
+        deleteTaskBtn.forEach(deleteTaskBtns => {
+            deleteTaskBtns.addEventListener('click', () => {
+                
+            });
+        });
+    }
+
+    const populateDetailDialog = (title, description, priority, date) => {
+        const taskDetailTitle = document.querySelector('.taskDetailTitle');
+
+        taskDetailTitle.textContent = title;
     };
 
     return {
